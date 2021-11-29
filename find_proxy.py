647859e703c3363b43d301a18ec8f799d4fcd819
jeff14994 <jeff14994@gmail.com>
2021-11-29 12:12:32 -0700

    Initialize Spinning Project

---
+++ find_proxy.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+def get_proxy_ip(url):
+    # 發起請求
+    response = requests.get(url)
+    # 剖析
+    soup = BeautifulSoup(response.text, "html.parser")
+    # print(soup)
+    # 使用 class 去取 table
+    table = soup.find("table", {"class": "table table-striped table-bordered"})
+    # 將該table 所有 td tag 取出來
+    # print(table)
+    all_proxy = table.findChildren('tr',recursive=True)
+   
+    # print(all_proxy)
+    # 將所有 IP 變成 list
+    all_ip = []
+    for i in range(len(all_proxy)):
+        try:
+            # 去除第一個欄位
+            if all_proxy[i].td == None:
+                continue
+            # 將所有 td tag 取出來 [:2] 表示取 td tag list 的前兩個元素（即 IP 與 port）
+            all_list = all_proxy[i].findChildren('td',recursive=True)[:2] 
+            # print(all_list)
+            # 將個別 ip:port 綁在一起成字串
+            ip = ""
+            for i in range(len(all_list)):
+                ip += str(all_list[i].text)
+                # 中間加 ":"
+                if(i == 0):
+                    ip += ":"
+            all_ip.append(ip)
+        except:
+            pass
+    # print(all_ip)
+    return all_ip
+if __name__ == "__main__":
+    url = "https://free-proxy-list.net/"
+    proxy_ips = get_proxy_ip(url)
+    print(len(proxy_ips))
---
+++ spin.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import find_proxy
+import random
+from  time import sleep
+from pwn import *
+import string
+from dotenv import load_dotenv
+load_dotenv()
+# Two steps
+# Step 1: Spin the wheel and get the coupon
+# Step 2: Check the coupon to see if it's 50% discount
+url = f"{os.environ['URL']}"
+headers = {}
+headers["Host"] = f"{os.environ['Host']}"
+headers["User-Agent"] = f"{os.environ['User-Agent']}"
+headers["Accept"] = f"{os.environ['Accept']}"
+headers["Accept-Language"] = f"{os.environ['Accept-Language']}"
+headers["Accept-Encoding"] = f"{os.environ['Accept-Encoding']}"
+headers["Content-Type"] = f"{os.environ['Content-Type']}"
+headers["Pf-Widget-Version"] = f"{os.environ['Pf-Widget-Version']}"
+headers["Content-Length"] = f"{os.environ['Content-Length']}"
+headers["Origin"] = f"{os.environ['Origin']}"
+headers["Connection"] = f"{os.environ['Connection']}"
+headers["Referer"] = f"{os.environ['Referer']}"
+headers["Sec-Fetch-Dest"] = f"{os.environ['Sec-Fetch-Dest']}"
+headers["Sec-Fetch-Mode"] = f"{os.environ['Sec-Fetch-Mode']}"
+headers["Sec-Fetch-Site"] = f"{os.environ['Sec-Fetch-Site']}"
+# print(headers)
+
+# print(data)
+# with open('json.file', "rb") as json_file:
+#     data = json_file.read()
+# print(data)
+# response = requests.get(url, headers = headers, data = data)
+proxy_url = "https://free-proxy-list.net/"
+proxy_ips = find_proxy.get_proxy_ip(proxy_url)
+# print(proxy_ips)
+def set_payload():
+    # Get data from json.file
+    data = {}
+    # Opening JSON file
+    with open('json.file') as json_file:
+        data = json.load(json_file)
+    payload_content = []
+    payload = string.ascii_letters
+    for i in payload:
+                payload_content.append(i)
+    random.shuffle(payload_content)
+    payload_content = "".join(payload_content)[:5]
+    # print(payload_content)
+    # Change email
+    data['inputs']["email"] = payload_content + payload_content[::-1][2:4] + "@gmail.com"
+    data['visitor_token'] = data['visitor_token'][:-2] + str(round(random.random() * 9))
+    # print(data)
+    data = json.dumps(data).encode('utf-8')
+    # print(data)
+    return data
+while True:
+    data = {}
+    data = set_payload()
+    try:
+        info("Start attack: ")
+        ip = random.choice(proxy_ips)
+        print('Use', ip)
+        r = requests.post(url, headers = headers, data = data, proxies={'http': ip, 'https': ip}, timeout=10)
+        info("Get responses: ")
+        response = r.text
+        print(response)
+        if len(response) < 1000:
+            with open("discount.txt", "a") as target:
+                target.write(response + "\n")
+        if "50% Discount" in response:
+            print("Got Good luck!!")
+            print(response)
+    except:
+        pass
